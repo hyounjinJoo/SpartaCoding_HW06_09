76efca03b5c3695aa10230a9b7bf4ff2bd100b1d
HyounJinDev <hyounjin.dev@gmail.com>
2025-02-03 20:24:42 +0900

    [Add] BasePawn 추가

---
+++ Source/HW06_07/Private/BasePawn.cpp
@@ -0,0 +1,17 @@
+﻿#include "BasePawn.h"
+
+ABasePawn::ABasePawn()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ABasePawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/HW06_07/Public/BasePawn.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BasePawn.generated.h"
+
+UCLASS()
+class HW06_07_API ABasePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABasePawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
