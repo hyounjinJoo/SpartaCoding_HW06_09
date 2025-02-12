ca2436e6e4922e1dead7c3aab02943161b52e026
HyounJinDev <hyounjin.dev@gmail.com>
2025-02-12 19:38:20 +0900

    [Add, Update] 아이템 인터페이스 추가 및 기존 플랫폼 인터페이스에 MinimalAPI 지정자 추가

---
+++ Source/HW06_07/Public/ItemInterface.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ItemInterface.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UItemInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class HW06_07_API IItemInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnItemBeginOverlap(AActor* OverlapActor) = 0;
+	virtual void OnItemEndOverlap(AActor* OverlapActor) = 0;
+	// 아이템 사용 시 호출
+	virtual void ActivateItem(AActor* Activator) = 0;
+	// "Coin", "Mine" 등 아이템 유형 반환
+	virtual FName GetItemType() const = 0;
+};
--- Source/HW06_07/Public/PlatformInterface.h
+++ Source/HW06_07/Public/PlatformInterface.h
@@ -1,13 +1,11 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
+﻿
 #pragma once
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
 #include "PlatformInterface.generated.h"
 
-// This class does not need to be modified.
-UINTERFACE()
+UINTERFACE(MinimalAPI)
 class UPlatformInterface : public UInterface
 {
 	GENERATED_BODY()
